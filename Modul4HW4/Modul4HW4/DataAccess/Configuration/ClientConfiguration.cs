830c5750958a65811f8e5692d40fecb236d484f8
OMIS\Geiko_J <rewers212>
2021-09-06 19:52:11 +0300

    HW Done!

--- Modul4HW4/Modul4HW4/DataAccess/Configuration/ClientConfiguration.cs
+++ Modul4HW4/Modul4HW4/DataAccess/Configuration/ClientConfiguration.cs
@@ -18,24 +18,6 @@ namespace Modul4HW4
             builder.Property(p => p.LastName).IsRequired().HasColumnName("LastName").HasMaxLength(50);
             builder.Property(p => p.Country).IsRequired().HasColumnName("Country").HasMaxLength(50);
             builder.Property(p => p.DateOfBirth).HasColumnName("DateOfBirth").HasColumnType("date");
-
-            builder.HasData(
-                new Client
-                {
-                    ClientId = 1,
-                    FirstName = "Tom",
-                    LastName = "Hanks",
-                    Country = "USA",
-                    DateOfBirth = new DateTime(1999, 11, 11)
-                },
-                new Client
-                {
-                    ClientId = 1,
-                    FirstName = "Britny",
-                    LastName = "Spirs",
-                    Country = "China",
-                    DateOfBirth = new DateTime(2000, 11, 11)
-                });
         }
     }
 }
---
+++ Modul4HW4/Modul4HW4/Migrations/20210906165147_SeedData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Modul4HW4.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Office",
+                columns: new[] { "Title", "Location", "Title1" },
+                values: new object[,]
+                {
+                    { 1, "USA", "USAOffice" },
+                    { 2, "Russia", "RussiaOffice" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Title",
+                columns: new[] { "TitleId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Developer" },
+                    { 2, "QA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "EmployeeId", "DateOfBirth", "FirstName", "HiredDate", "LastName", "OfficeId", "TitleId" },
+                values: new object[] { 1, new DateTime(1961, 8, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Tom", new DateTime(1985, 11, 21, 9, 30, 12, 0, DateTimeKind.Unspecified), "Soyer", 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "EmployeeId", "DateOfBirth", "FirstName", "HiredDate", "LastName", "OfficeId", "TitleId" },
+                values: new object[] { 2, new DateTime(1993, 2, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Avram", new DateTime(2015, 8, 1, 11, 22, 1, 0, DateTimeKind.Unspecified), "Linkoln", 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "EmployeeProject",
+                columns: new[] { "EmployeeProjectId", "EmployeeId", "ProjectId", "Rate", "StartedDate" },
+                values: new object[] { 1, 1, 1, 1000m, new DateTime(1999, 11, 11, 11, 30, 22, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "EmployeeProject",
+                columns: new[] { "EmployeeProjectId", "EmployeeId", "ProjectId", "Rate", "StartedDate" },
+                values: new object[] { 2, 2, 2, 2000m, new DateTime(1777, 7, 1, 11, 22, 11, 0, DateTimeKind.Unspecified) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EmployeeProject",
+                keyColumn: "EmployeeProjectId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "EmployeeProject",
+                keyColumn: "EmployeeProjectId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "EmployeeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "EmployeeId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Office",
+                keyColumn: "Title",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Office",
+                keyColumn: "Title",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Title",
+                keyColumn: "TitleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Title",
+                keyColumn: "TitleId",
+                keyValue: 2);
+        }
+    }
+}
